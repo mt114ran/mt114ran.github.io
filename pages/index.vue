0ff3d8b5c5c5f6c1843066f25176bb1103c6e32e
maitatomoya <mt114r.an@gmail.com>
2025-05-29 15:40:31 +0900

    初回コミット: Nuxt ブログ構成

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>記事一覧</h1>
+    <p v-if="!articles || articles.length === 0">記事が見つかりませんでした。</p>
+    <ul>
+      <li v-for="article in articles" :key="article._path">
+        <pre>{{ article }}</pre>
+        <NuxtLink :to="article._path">{{ article.title || article._path }}</NuxtLink>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup lang="ts">
+const { data: articles } = await useAsyncData('articles', () =>
+  queryContent().find() // ← only() を外す
+)
+</script>
