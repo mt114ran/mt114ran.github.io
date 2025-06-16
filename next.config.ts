cef1780b3a2552c89f8cd582f0bbca56ac76f847
maitatomoya <mt114r.an@gmail.com>
2025-06-16 18:20:02 +0900

    fix: next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -8,7 +8,7 @@ const nextConfig: NextConfig = {
   output: 'export',         // 静的エクスポートの指定
   trailingSlash: true,      // GitHub Pagesでルーティングを正しく動かす
   basePath: '',             // ユーザーサイトの場合は空のままでOK
-  assetPrefix: isGithubPages ? `/${repoName}/` : '',
+  assetPrefix: isGithubPages ? `/` : '',
 }
 
 export default nextConfig
