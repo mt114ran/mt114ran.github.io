c30d383435a64ddd96cb479fb3971957ecca2f56
maitatomoya <mt114r.an@gmail.com>
2025-06-16 17:58:23 +0900

    feat: edit next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,14 @@
 import type { NextConfig } from 'next'
 
+const isGithubPages = process.env.NODE_ENV === 'production'
+const repoName = 'mt114ran.github.io' // あなたのリポジトリ名に応じて変更
+
 /** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
   output: 'export',         // 静的エクスポートの指定
   trailingSlash: true,      // GitHub Pagesでルーティングを正しく動かす
   basePath: '',             // ユーザーサイトの場合は空のままでOK
-  assetPrefix: '',
+  assetPrefix: isGithubPages ? `/${repoName}/` : '',
 }
 
 export default nextConfig
