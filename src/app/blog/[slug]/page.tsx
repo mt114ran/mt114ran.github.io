398ad3d2e338dcbc23f1472228611d5f4a5f8642
maitatomoya <mt114r.an@gmail.com>
2025-08-02 23:03:54 +0900

    fix: 記事ページのidのcolorを変更

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -14,7 +14,7 @@ export default async function BlogPostPage(props: any) {
   return (
     <div className="max-w-2xl mx-auto py-10 px-4">
       <h1 className="text-3xl font-bold mb-2">
-        <span className="text-gray-600">#{post.id}</span> {post.title}
+        <span className="text-green-600">#{post.id}</span> {post.title}
       </h1>
       <ul className="list-none border-b border-gray-600 pb-4 mb-6">
         {post.create && (
